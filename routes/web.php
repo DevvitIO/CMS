23f6e7757c28ab3131cdef9c5235b52b66c63af7
Dillon Lomnitzer <yodaesu@gmail.com>
2018-01-17 21:35:04 -0500

    Admin/Public Route groups

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,16 @@
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pub.index');
+})->name('pub.index');
+
+Route::group(['prefix' => 'admin'], function(){
+
+    Route::get('dashboard', function() {
+        return view('admin.index');
+    })->name('admin.index');
+
 });
