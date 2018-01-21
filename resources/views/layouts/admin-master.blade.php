f8c7fe73c6ac7430ed593a11f3dce08e96cd5bb0
Dillon Lomnitzer <yodaesu@gmail.com>
2018-01-21 11:56:50 -0500

    Separate header for public view

--- resources/views/layouts/admin-master.blade.php
+++ resources/views/layouts/admin-master.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Site Status Checker</title>
+        <title>Devvit.IO</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -16,7 +16,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
         <!-- Custom CSS -->
-        <link rel="stylesheet" href="{{ URL::to('css/styles.css') }}">
+        <link rel="stylesheet" href="{{ URL::to('css/admin.css') }}">
     </head>
     <body>
     @include('partials.admin-header')
