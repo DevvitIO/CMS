07a8d96e4178be78785f91a5c0d68ede7302b31b
Dillon Lomnitzer <yodaesu@gmail.com>
2018-01-26 08:04:26 -0500

    Hard Code URL to View Identifier

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/admin/dashboard';
+    protected $redirectTo = 'admin.index';
 
     /**
      * Create a new controller instance.
