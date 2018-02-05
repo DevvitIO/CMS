909991fe7b247295288793046f13a7590d208aef
nemanjamilosevic <nemanjamilosevic29@yahoo.com>
2018-02-05 22:25:51 +0100

    Initial Admin Dashboard FE

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = 'admin.index';
+    protected $redirectTo = 'admin/dashboard';
 
     /**
      * Create a new controller instance.
--- public/js/admin-scripts.js
+++ public/js/admin-scripts.js
@@ -9,3 +9,17 @@ li.each(function(){
 		});
 	};
 });
+
+
+
+ $(document).ready(function () {
+                $("#sidebar").mCustomScrollbar({
+                    theme: "minimal"
+                });
+
+                $('#sidebarCollapse').on('click', function () {
+                    $('#sidebar, #content').toggleClass('active');
+                    $('.collapse.in').toggleClass('in');
+                    $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+                });
+            });
